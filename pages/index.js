ba59dca8c0860d802c16a88201a96f5c83492620
Edmund Ju <edmundju@berkeley.edu>
2023-05-29 17:30:38 -0700

    add user input box

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("")
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT-3 Writer</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Feel Supported, Whenever You Need It</h1>
           </div>
           <div className="header-subtitle">
             <h2>insert your subtitle here</h2>
           </div>
         </div>
+        <div className='prompt-container'>
+          <textarea placeholder='start-typing-here' className='prompt-box' value={userInput} onChange={(e)=>setUserInput(e.target.value)}></textarea>
+        </div>
+
       </div>
       <div className="badge-container grow">
         <a
